3139d36b14c3cc90b5167dd965524f699f4e6358
minhajul-karim <minhajul.kaarim@gmail.com>
2020-05-21 16:42:54 +0600

    standardize js

--- application.py
+++ application.py
@@ -2,11 +2,17 @@
 
 import os
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory, redirect, url_for, send_file
 from flask_socketio import SocketIO, emit, send, join_room, leave_room
+from werkzeug.utils import secure_filename
+from binascii import a2b_base64
+
+UPLOAD_FOLDER = "static/uploads"
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 socketio = SocketIO(app)
 
 # Store chats, channels
@@ -29,7 +35,7 @@ def get_name():
 @app.route("/create_room", methods=["POST"])
 def create_room():
     """Create a new room."""
-    new_room = request.form.get("new_room")
+    new_room = request.form.get("newRoom")
 
     # Return false if room already exists, otherwise return true
     if new_room in rooms:
@@ -38,6 +44,40 @@ def create_room():
         rooms.append(new_room)
         chats[new_room] = []
         return jsonify({"status": True})
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload_file():
+    """Upload file."""
+    if request.method == "POST":
+        # Check if the post request has the file part
+        if 'file' not in request.files:
+            return "No file part"
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == "":
+            return "No file selected"
+        if not allowed_file(file.filename):
+            return "File format not supported"
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.root_path,
+                                   app.config["UPLOAD_FOLDER"], filename))
+            return redirect(url_for("uploaded_file", filename=filename))
+    return render_template("file_form.html")
+
+
+@app.route("/uploads/<filename>")
+def uploaded_file(filename):
+    file = os.path.join(app.root_path,
+                        app.config["UPLOAD_FOLDER"], filename)
+    return send_file(file, as_attachment=True)
+
+
+def allowed_file(filename):
+    """Check allowed filename."""
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @socketio.on("message")
@@ -84,8 +124,25 @@ def leave(data):
 @socketio.on("new room")
 def new_room(data):
     """Emit new room."""
-    new_room = data["new_room"]
-    emit("display new room", {"new_room": new_room}, broadcast=True)
+    new_room = data["newRoom"]
+    emit("display new room", {"room": new_room}, broadcast=True)
+
+
+@socketio.on("file_attachment")
+def file_attachment(data):
+    """Emit file."""
+    image_data = data["imageData"]
+    print(image_data)
+    binary_data = a2b_base64(image_data)
+
+    fd = open("test.bmp", "wb")
+    fd.write(binary_data)
+    fd.close()
+    # file = os.path.join(app.root_path,
+    #                     app.config["UPLOAD_FOLDER"], "API.png")
+    # with open(file, 'rb') as f:
+    #     image_data = f.read()
+    # emit("my-image-event", {"image_data": image_data}, broadcast=True)
 
 
 if __name__ == "__main__":
--- static/js/index.js
+++ static/js/index.js
@@ -1,227 +1,258 @@
 // Template for an individual message
-const message_template = Handlebars.compile(
-    document.querySelector("#message-template").innerHTML
+let messageTemplate = Handlebars.compile(
+  document.querySelector('#message-template').innerHTML
 );
 
 // Template for chat history of a room
-const chat_history_template = Handlebars.compile(
-    document.querySelector("#chat-history").innerHTML
+let chatHistoryTemplate = Handlebars.compile(
+  document.querySelector('#chat-history').innerHTML
 );
 
 // When the DOM is loaded
-document.addEventListener("DOMContentLoaded", () => {
-    // Display the user name
-    const name = localStorage.getItem("name");
-    let last_visited_room = localStorage.getItem("last_visited_room");
-    document.querySelector("#username").innerHTML += `${name}`;
+document.addEventListener('DOMContentLoaded', () => {
+  // Display the user name
+  let name = localStorage.getItem('name');
+  let lastRoom = localStorage.getItem('lastRoom');
+  document.querySelector('#username').innerHTML += `${name}`;
 
-    // Connect to websocket
-    let socket = io.connect(
-        location.protocol + "//" + document.domain + ":" + location.port
-    );
+  // Connect to websocket
+  let socket = io.connect(
+    location.protocol + '//' + document.domain + ':' + location.port
+  );
 
-    // Join last visted room when connected
-    socket.on("connect", () => {
-        // Join the last room user last visited
-        if (!last_visited_room) {
-            last_visited_room = "general";
-            localStorage.setItem("last_visited_room", last_visited_room);
+  // Join last visted room when connected
+  socket.on('connect', () => {
+    // Join the last room user last visited
+    if (!lastRoom) {
+      lastRoom = 'general';
+      localStorage.setItem('lastRoom', lastRoom);
 
-            // Display general as an active room
-            document.getElementsByClassName("room")[0].className += " active";
-        } else {
-            // Make the last visited room active
-            let all_rooms = document.getElementsByClassName("room");
-            for (let i = 0; i < all_rooms.length; i++) {
-                if (all_rooms[i].innerHTML == last_visited_room) {
-                    all_rooms[i].className += " active";
-                    break;
-                }
-            }
+      // Display general as an active room
+      document.getElementsByClassName('room')[0].className += ' active';
+    } else {
+      // Make the last visited room active
+      let rooms = document.getElementsByClassName('room');
+      for (let i = 0; i < rooms.length; i++) {
+        if (rooms[i].innerHTML === lastRoom) {
+          rooms[i].className += ' active';
+          break;
         }
-        join_room(last_visited_room);
-    });
-
-    // Enable enter key to send messages
-    let text_area = document.querySelector("#message");
-    text_area.addEventListener("keyup", (event) => {
-        event.preventDefault();
-
-        // When ENTER key pressed without shift key
-        if (event.keyCode == 13 && !event.shiftKey) {
-            let message = document.querySelector("#message").value;
-            let current_date = new Date();
-            let current_time = current_date.toUTCString();
-            let time =
-                current_time.slice(0, 4) +
-                " " +
-                current_time.slice(17, 22) +
-                " " +
-                current_time.slice(-3);
-
-            socket.send({
-                name: name,
-                message: message,
-                time: time,
-                room: last_visited_room,
-            });
-            // Clear the text area
-            document.querySelector("#message").value = "";
-        }
-        // Display multi-line message instruction
-        let message_length = document.querySelector("#message").value.length;
-        if (message_length > 0) {
-            document.querySelector("#multiline-instruction").style.display =
-                "block";
-        } else {
-            document.querySelector("#multiline-instruction").style.display =
-                "none";
-        }
-    });
-
-    // Display message
-    socket.on("message", (data) => {
-        const content = message_template({
-            name: data.name,
-            message: data.message,
-            time: data.time,
-        });
-        document.querySelector("#message-area").innerHTML += content;
-        move_to_bottom();
-    });
-
-    // Click on a room name to join
-    document.querySelector("#rooms-list").addEventListener("click", (event) => {
-        // Event delegation
-        if (event.target && event.target.nodeName == "LI") {
-            let new_room = event.target.innerHTML;
-            if (new_room != last_visited_room) {
-                leave_room(last_visited_room);
-                join_room(new_room);
-                last_visited_room = new_room; // Redundant? As join_room() updates last_visited_room
-
-                // Remove previous active class
-                inactive();
-
-                // Make current LI active
-                event.target.className += " active";
-            }
-        }
-    });
-
-    // Join room
-    function join_room(room) {
-        socket.emit("join", { name: name, room: room });
-
-        // Set last visited room
-        last_visited_room = room;
-        localStorage.setItem("last_visited_room", room);
+      }
     }
+    join_room(lastRoom);
+  });
 
-    // Generate chat history
-    socket.on("chat history", (data) => {
-        // Clear previous chat header and chats
-        document.querySelector("#chat-header").innerHTML = "";
-        document.querySelector("#message-area").innerHTML = "";
+  // Enable enter key to send messages
+  let textArea = document.querySelector('#message');
+  textArea.addEventListener('keyup', (event) => {
+    event.preventDefault();
 
-        // Display room name at the top
-        const p_element = document.createElement("p");
-        p_element.appendChild(document.createTextNode(`${last_visited_room}`));
-        document.querySelector("#chat-header").appendChild(p_element);
+    // When ENTER key pressed without shift key
+    if (event.keyCode === 13 && !event.shiftKey) {
+      let message = document.querySelector('#message').value;
+      let curDate = new Date();
+      let utcTime = curDate.toUTCString();
+      let time =
+        utcTime.slice(0, 4) +
+        ' ' +
+        utcTime.slice(17, 22) +
+        ' ' +
+        utcTime.slice(-3);
 
-        // Place an <hr> after room name
-        const hr_element = document.createElement("hr");
-        document.querySelector("#chat-header").appendChild(hr_element);
+      socket.send({
+        name: name,
+        message: message,
+        time: time,
+        room: lastRoom,
+      });
+      // Clear the text area
+      document.querySelector('#message').value = '';
+    }
+    // Display multi-line message instruction
+    let textLen = document.querySelector('#message').value.length;
+    if (textLen > 0) {
+      document.querySelector('#multiline-instruction').style.display = 'block';
+    } else {
+      document.querySelector('#multiline-instruction').style.display = 'none';
+    }
+  });
 
-        // Feed data to chat history template
-        const content = chat_history_template({ chats: data["chats"] });
-        document.querySelector("#message-area").innerHTML += content;
-
-        // Scroll to the bottom of corversation
-        move_to_bottom();
+  // Display message
+  socket.on('message', (data) => {
+    let content = messageTemplate({
+      name: data.name,
+      message: data.message,
+      time: data.time,
     });
+    document.querySelector('#message-area').innerHTML += content;
+    goDown();
+  });
 
-    // Move to the end of conversation
-    function move_to_bottom() {
-        const element = document.querySelector("#message-area");
-        element.scrollTop = element.scrollHeight - element.clientHeight;
-    }
-
-    // Leave room
-    function leave_room(room) {
-        socket.emit("leave", { name: name, room: room });
-    }
-
-    // Create new room
-    document.querySelector("#new-room-form").onsubmit = () => {
-        // Initialize a new request
-        const request = new XMLHttpRequest();
-        const new_room = document.querySelector("#room-name").value;
-        request.open("POST", "/create_room");
-
-        // Callback function when the request is complete
-        request.onload = () => {
-            // Extract data received from server
-            const data = JSON.parse(request.responseText);
-
-            if (request.status >= 200 && request.status < 300) {
-                if (data.status == true) {
-                    socket.emit("new room", {
-                        new_room: new_room,
-                    });
-
-                    // Join new room
-                    join_room(new_room);
-
-                    // Click the close button
-                    document.querySelector("#close-button").click();
-                } else {
-                    document.querySelector("#room-error").style.display =
-                        "block";
-                }
-            } else {
-                console.log(Error("Can not connect."));
-            }
-        };
-
-        // Send data with the request
-        const data = new FormData();
-        data.append("new_room", new_room);
-        request.send(data);
-        return false;
-    };
-
-    // Add new rooom to DOM
-    socket.on("display new room", (data) => {
-        let newly_created_room = data.new_room;
-        const li = document.createElement("li");
-        li.appendChild(document.createTextNode(newly_created_room));
-        li.className = "room";
-        document.querySelector("#rooms-list").append(li);
+  // Click on a room name to join
+  document.querySelector('#rooms-list').addEventListener('click', (event) => {
+    // Event delegation
+    if (event.target && event.target.nodeName === 'LI') {
+      let newRoom = event.target.innerHTML;
+      if (newRoom != lastRoom) {
+        leave_room(lastRoom);
+        join_room(newRoom);
+        lastRoom = newRoom; // Redundant? As join_room() updates lastRoom
 
         // Remove previous active class
-        if (newly_created_room == localStorage.getItem("last_visited_room")) {
-            inactive();
-            li.className += " active";
+        deactivate();
+
+        // Make current LI active
+        event.target.className += ' active';
+      }
+    }
+  });
+
+  // Join room
+  function join_room(room) {
+    socket.emit('join', { name: name, room: room });
+
+    // Set last visited room
+    lastRoom = room;
+    localStorage.setItem('lastRoom', room);
+  }
+
+  // Generate chat history
+  socket.on('chat history', (data) => {
+    // Clear previous chat header and chats
+    document.querySelector('#chat-header').innerHTML = '';
+    document.querySelector('#message-area').innerHTML = '';
+
+    // Display room name at the top
+    let p = document.createElement('p');
+    p.appendChild(document.createTextNode(`${lastRoom}`)); // Use textContent?
+    document.querySelector('#chat-header').appendChild(p);
+
+    // Place an <hr> after room name
+    let hr = document.createElement('hr');
+    document.querySelector('#chat-header').appendChild(hr);
+
+    // Feed data to chat history template
+    let content = chatHistoryTemplate({ chats: data['chats'] });
+    document.querySelector('#message-area').innerHTML += content;
+
+    // Scroll to the bottom of corversation
+    goDown();
+  });
+
+  // Move to the end of conversation
+  function goDown() {
+    let element = document.querySelector('#message-area');
+    element.scrollTop = element.scrollHeight - element.clientHeight;
+  }
+
+  // Leave room
+  function leave_room(room) {
+    socket.emit('leave', { name: name, room: room });
+  }
+
+  // Create new room
+  document.querySelector('#new-room-form').onsubmit = () => {
+    // Initialize a new request
+    let request = new XMLHttpRequest();
+    let newRoom = document.querySelector('#room-name').value;
+    request.open('POST', '/create_room');
+
+    // Callback function when the request is complete
+    request.onload = () => {
+      // Extract data received from server
+      let data = JSON.parse(request.responseText);
+
+      if (request.status >= 200 && request.status < 300) {
+        if (data.status === true) {
+          socket.emit('new room', {
+            newRoom: newRoom,
+          });
+
+          // Join new room
+          join_room(newRoom);
+
+          // Click the close button
+          document.querySelector('#close-button').click();
+        } else {
+          document.querySelector('#room-error').style.display = 'block';
         }
-    });
+      } else {
+        console.log(Error('Can not connect.'));
+      }
+    };
 
-    // Logout
-    document.querySelector("#logout").addEventListener("click", () => {
-        localStorage.clear();
-        window.location.href = "/sign_in";
-    });
+    // Send data with the request
+    let data = new FormData();
+    data.append('newRoom', newRoom);
+    request.send(data);
+    return false;
+  };
 
-    function inactive() {
-        let current_active = document.getElementsByClassName("active");
-        current_active[0].className = "room";
+  // Add new rooom to DOM
+  socket.on('display new room', (data) => {
+    let newRoom = data.room;
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(newRoom));
+    li.className = 'room';
+    document.querySelector('#rooms-list').append(li);
+
+    // Remove previous active class
+    if (newRoom === localStorage.getItem('lastRoom')) {
+      deactivate();
+      li.className += ' active';
     }
+  });
 
-    // Notify users
-    function notify(message) {
-        const p_element = document.createElement("p");
-        p_element.innerHTML = message;
-        p_element.className = "text-muted";
-        document.querySelector("#message-area").append(p_element);
+  // Logout
+  document.querySelector('#logout').addEventListener('click', () => {
+    localStorage.clear();
+    window.location.href = '/sign_in';
+  });
+
+  function deactivate() {
+    let current_active = document.getElementsByClassName('active');
+    current_active[0].className = 'room';
+  }
+
+  // Notify users
+  function notify(message) {
+    let p = document.createElement('p');
+    p.innerHTML = message;
+    p.className = 'text-muted';
+    document.querySelector('#message-area').append(p);
+  }
+
+  // document.querySelector("#image").addEventListener("click", () => {
+  //     socket.emit("file_attachment", { room: lastRoom });
+  // });
+
+  // socket.on("my-image-event", (data) => {
+  //     console.log(data);
+  //     document.querySelector("#message-area").innerHTML = data["imageData"];
+  // });
+
+  // Files
+  let fileSelector = document.querySelector('#file-selector');
+  fileSelector.addEventListener('change', (event) => {
+    let filesList = event.target.files;
+    let file = filesList[0];
+    let output = document.querySelector('#output');
+
+    if (window.File && window.FileReader) {
+      let reader = new FileReader();
+      reader.addEventListener('load', (event) => {
+        console.log(event.target.result);
+        output.src = event.target.result;
+        socket.emit('file_attachment', {
+          imageData: event.target.result,
+          room: lastRoom,
+        });
+      });
+      reader.readAsDataURL(file);
+    } else {
+      alert(
+        "Your Browser Doesn't Support The File API Please Update Your Browser"
+      );
     }
+  });
 });
